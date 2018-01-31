ed12fa1c23a32caf88511f5d2aabcea88a312c47
DESKTOP-KP5J51F\maria <mariatopircean34@gmail.com>
2018-01-31 23:39:48 +0000

    added the beginning of the hover mechanic

---
+++ Assets/Scripts/OnMouseHover.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnMouseHover : MonoBehaviour
+{
+
+    public Text HoverSpeciesName;
+    public float ReachRay;
+    public Camera MainCamera;
+    bool _wasDiscovered;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+            RaycastHit RaycastHit;
+            if (Physics.Raycast(MainCamera.transform.position, MainCamera.transform.forward, out RaycastHit, 7))
+            {
+                SpeciesBase animalHit = RaycastHit.transform.GetComponent<SpeciesBase>();
+                if (animalHit != null)
+                {
+                HoverSpeciesName.gameObject.SetActive(true);
+                }
+            }
+        }
+    
+}
