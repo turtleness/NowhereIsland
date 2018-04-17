d8ae4b8ec4b40b9a9b6c6fc579892dd258c444df
Maria <mariatopircean34@gmail.com>
2018-04-17 15:27:05 +0100

    did more groundpainting

---
+++ Assets/Scripts/CullDistances.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CullDistances : MonoBehaviour {
+
+    public float[] LayerDistances = new float[32];
+
+    void Start()
+    {
+        Camera camera = GetComponent<Camera>();
+        for(int i=0;i<LayerDistances.Length; i++)
+        {
+            if (LayerDistances[i] == 0) LayerDistances[i] = camera.farClipPlane;
+        }
+
+        camera.layerCullDistances = LayerDistances;
+    }
+}
